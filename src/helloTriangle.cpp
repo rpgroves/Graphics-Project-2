3bad0cf5460d2e6727be0c625518af73a60ca667
Brody <brody.foster@gmail.com>
2024-03-06 20:32:39 -0500

    i am awesome. the square is far.

--- src/helloTriangle.cpp
+++ src/helloTriangle.cpp
@@ -195,7 +195,7 @@ int main()
         glm::mat4 projection    = glm::mat4(1.0f);
 
         //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -30.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         
         loadedShader.setMat4("model", model);
